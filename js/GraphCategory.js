4df433c3343bd0806d93d2b729efd5fc9da30f19
Kurt DaCosta <kurtbradd@gmail.com>
2015-05-12 11:05:46 -0400

    new parser for binned data

--- js/GraphCategory.js
+++ js/GraphCategory.js
@@ -6,6 +6,11 @@ function GraphCategory (name, data, data_type) {
 var parseData = function (data, data_type) {
 	if (data_type === "linear") 	return _.map(data, linearData);
 	if (data_type === "datetime") return _.map(data, datetimeData);
+	if (data_type === "binned") 	return _.map(data, binData);
+}
+
+var binData = function (e) {
+	return [e.x_val, parseFloat(e.y_val)];
 }
 
 var linearData = function (e) {
