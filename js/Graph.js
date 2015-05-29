3764ab37f51219295f5f8ba134bc8116e0e8525c
Kurt DaCosta <kurtbradd@gmail.com>
2015-05-29 17:48:03 -0400

    handle graphs with labels and categories

--- js/Graph.js
+++ js/Graph.js
@@ -9,20 +9,21 @@ function Graph (attributes, data) {
 
 Graph.prototype.drawGraph = function() {
 	var base_config = {
+		series: 	this.categories,
 		legend: 	{ title: { text: this.labels.category }},
 		xAxis: 		{ title: { text: this.labels.x_axis 	}},
 		yAxis: 		{ title: { text: this.labels.y_axis 	}},
 	}
 	var graph_config = _.merge(base_config, this.attributes);
 	var chart = new Highcharts.Chart(graph_config);
-	
-	var categories = this.categories;
-	var count = 0;
-	chart.addSeries(categories[count++]);
-	var i = setInterval(function(){
-    chart.addSeries(categories[count++]);
-    if(count === categories.length) clearInterval(i);
-	}, 1100);
+
+	// var categories = this.categories;
+	// var count = 0;
+	// chart.addSeries(categories[count++]);
+	// var i = setInterval(function(){
+ //    chart.addSeries(categories[count++]);
+ //    if(count === categories.length) clearInterval(i);
+	// }, 1100);
 
 };
 
@@ -37,14 +38,25 @@ var parseDataPoints = function (categories) {
 
 var pointFromData = function (point) {
 	return {
+		label: point[this.labels.label],
 		x_val: point[this.labels.x_axis],
 		y_val: point[this.labels.y_axis],
 	};
 }
 
 var labelsFromHeader = function (header) {
+	if (header.length == 4) {
+		return {
+			category: header[0],
+			label: header[1],
+			x_axis: header[2],
+			y_axis: header[3]
+		}
+	}
+
 	return {
 		category: header[0],
+		label: header[0],
 		x_axis: header[1],
 		y_axis: header[2]
 	}
